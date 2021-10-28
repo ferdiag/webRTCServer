6af40b34b26337d5416d27816a28ac1b51b6e5cc
ferhat <ferdiag@yahoo.de>
2021-10-28 07:58:37 +0200

    update comments

--- client/src/components/VideoContainer.js
+++ client/src/components/VideoContainer.js
@@ -3,13 +3,13 @@ import { AppContext } from '../context/AppContext'
 
 const VideoContainer = () => {
 
-    //This component is a container for the streams received at the
-    // app context and will only be shown if a videoconference has been started.
-    // parent: App.js
     
-    const { videoContainerRef,isVideoConference } = useContext(AppContext)
+    // will only be shown if a videoconference has been started.
+    // parent: App.js
+
+    const { videoContainerRef, isVideoConference } = useContext(AppContext)
     return (
-        <div style={!isVideoConference ? {display:"none"} :{
+        <div style={!isVideoConference ? { display: "none" } : {
             height: "500px",
             width: "500px",
         }}
--- client/src/socket/index.js
+++ client/src/socket/index.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
-   //   export const socket = io('http://192.168.45.1:4000',);
-     export const socket = io('http://192.168.0.239:4000');
+     export const socket = io('http://192.168.44.241:4000',);
+   //   export const socket = io('http://192.168.0.239:4000');
 //  export const socket = io('http://192.168.178.33:4000',);
    // export const socket = io('http://192.168.2.137:4000')
   // export const socket = io('http://192.168.178.33:4000');
